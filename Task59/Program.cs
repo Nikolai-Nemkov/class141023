b29002eaa176e3342d53fb4a5ecdaabcc59a3f61
Николай <nnm_58@mail.ru>
2023-10-14 17:25:37 +0300

    Задача 59

--- Task59/Program.cs
+++ Task59/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine();
+Console.WriteLine("Задача 59");
+Console.WriteLine();
 
 // Задача 59:
 // Задайте двумерный массив из целых чисел.
@@ -24,7 +26,6 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
         {
             matrix[i, j] = rnd.Next(min, max + 1);
         }
-        
     }
 
     return matrix;
@@ -32,7 +33,6 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 
 void PrintMatrix(int[,] matrix)
 {
-    
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         Console.Write("|");
@@ -42,7 +42,6 @@ void PrintMatrix(int[,] matrix)
         }
         Console.WriteLine("    |");
     }
-   
 }
 
 int[] GetIndexesMinElem(int[,] matrix)
@@ -51,28 +50,29 @@ int[] GetIndexesMinElem(int[,] matrix)
     int columnMin = 0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-       for (int j = 0; j < matrix.GetLength(1); j++) 
-       {
-            if (matrix[i,j] <=matrix[rowMin, columnMin])
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] <= matrix[rowMin, columnMin])
             {
                 rowMin = i;
                 columnMin = j;
             }
-       }
+        }
     }
-    return new int[] {rowMin, columnMin};
-} 
-int [,] DeleteRowColumnMinElem(int[,] matrix, int delRow, int delColumn)
+    return new int[] { rowMin, columnMin };
+}
+int[,] DeleteRowColumnMinElem(int[,] matrix, int delRow, int delColumn)
 {
-    int[,] littleMatrix = new int[matrix.GetLength(0)-1, matrix.GetLength(1)-1];
+    int[,] littleMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
     for (int i = 0, m = 0; i < littleMatrix.GetLength(0); i++, m++)
     {
-        if (i == delRow) m++;
-        for (int j = 0, n = 0; j < littleMatrix.GetLength(1); j++,n++)
+        if (i == delRow)
+            m++;
+        for (int j = 0, n = 0; j < littleMatrix.GetLength(1); j++, n++)
         {
-            if (j == delColumn) n++;
-            littleMatrix[i, j] = matrix[m,n];
-
+            if (j == delColumn)
+                n++;
+            littleMatrix[i, j] = matrix[m, n];
         }
     }
     return littleMatrix;
@@ -81,6 +81,11 @@ int [,] DeleteRowColumnMinElem(int[,] matrix, int delRow, int delColumn)
 int[,] array2d = CreateMatrixRndInt(5, 5, -10, 10);
 PrintMatrix(array2d);
 int[] indexesMinElem = GetIndexesMinElem(array2d);
-int[,] deleteRowColumnMinElem = DeleteRowColumnMinElem(array2d, indexesMinElem[0], indexesMinElem[1]);
+int[,] deleteRowColumnMinElem = DeleteRowColumnMinElem(
+    array2d,
+    indexesMinElem[0],
+    indexesMinElem[1]
+);
 Console.WriteLine();
 PrintMatrix(deleteRowColumnMinElem);
+Console.WriteLine();
