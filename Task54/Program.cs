000e51aaa3c919692a9fcd109ee1d95402f8bc75
Николай <nnm_58@mail.ru>
2023-10-14 15:59:14 +0300

    Задача 54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine();
+Console.WriteLine("Задача 54");
+Console.WriteLine();
 
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
 // каждой строки двумерного массива.
@@ -13,12 +15,15 @@ Console.WriteLine("Hello, World!");
 int m = InputNumbers("Введите количество столбцов, m: ");
 int n = InputNumbers("Введите количество строк, n: ");
 int range = InputNumbers("Введите диапазон значений: от 1 до ");
+Console.WriteLine();
 
 int[,] array = new int[m, n];
 CreateArray(array);
 PrintMatrix(array);
+Console.WriteLine();
 
 Console.WriteLine($"\nОтсортированный массив: ");
+Console.WriteLine();
 SortRowsDescending(array);
 PrintMatrix(array);
 
